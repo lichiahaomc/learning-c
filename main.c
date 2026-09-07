99a8786a946f6ba91883ceac5a969ed32cd19579
lichiahaomc <lichiahao_mc@163.com>
2026-09-08 00:22:17 +0800

    学习了函数并接触到了一点局部/全局变量的情况

--- main.c
+++ main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
+int add(int a, int b);
+
 int main(void)
 {
-    int i = 0;
-    
-    // 循环
-    do
-    {
-	printf("%d\n", i);
-	i++;
-    }
-    while (i < 10);
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d + %d = %d\n", a, b, add(a, b));
 
     return 0;
+}
+
+int add(int a, int b)
+{
+    return a + b;
 }
