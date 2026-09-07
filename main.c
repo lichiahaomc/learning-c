d33738d2cd53a12aaf0b9b48a7b6ca3289faf891
lichiahaomc <lichiahao_mc@163.com>
2026-09-07 20:06:51 +0800

    学习了 switch 的用法

--- main.c
+++ main.c
@@ -2,13 +2,19 @@
 
 int main(void)
 {
-    int a = 10;
+    char grade = 'B';
 
-    if (a == 10) printf("Yes\n");
-    else printf("No\n");
-
-    // the same
-    (a == 10) ? printf("Yes\n") : printf("No\n"); 
+    switch (grade)
+    {
+	case 'A': printf("很棒\n"); break;
+	case 'B': printf("非常棒\n"); break;
+	case 'C': printf("做得好\n"); break;
+	case 'D': printf("您通过了\n"); break;
+	case 'E': printf("最好再试一下\n"); break;
+	case 'F': printf("emmm...\n"); break;
+	default: printf("无效成绩\n"); break;
+    }
+    printf("您的成绩是：%c\n", grade);
 
     return 0;
 }
