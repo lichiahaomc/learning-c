34d5afa7f58084b7bc3f06ac3cea58f034412e44
lichiahaomc <lichiahao_mc@163.com>
2026-09-07 17:12:51 +0800

    学习数据类型转换

--- main.c
+++ main.c
@@ -2,25 +2,8 @@
 
 int main(void)
 {
-    // find the max one
-    int a, b, c;
-    int max;
-
-    scanf("%d %d %d", &a, &b, &c);
-
-    if (a < b)
-    {
-	if (b < c) max = c;
-	else max = b;
-    }
-    else if (a > b)
-    {
-	if (a < c) max = c;
-	else max = a;
-    }
-    else printf("ERROR: Cannot find the max one.\n");
-
-    printf("The max one is %d.\n", max);
+    int a = 10;
+    printf("%lf\n", (double)a);
 
     return 0;
 }
