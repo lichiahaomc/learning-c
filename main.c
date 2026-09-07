0e27f09f14f4a4c486f28d13a18c0b99efd953ac
lichiahaomc <lichiahao_mc@163.com>
2026-09-07 20:09:18 +0800

    学习了 for 循环

--- main.c
+++ main.c
@@ -2,19 +2,11 @@
 
 int main(void)
 {
-    char grade = 'B';
-
-    switch (grade)
+    // 循环
+    for (int i = 0; i < 10; i++)
     {
-	case 'A': printf("很棒\n"); break;
-	case 'B': printf("非常棒\n"); break;
-	case 'C': printf("做得好\n"); break;
-	case 'D': printf("您通过了\n"); break;
-	case 'E': printf("最好再试一下\n"); break;
-	case 'F': printf("emmm...\n"); break;
-	default: printf("无效成绩\n"); break;
+	printf("%d\n", i);
     }
-    printf("您的成绩是：%c\n", grade);
 
     return 0;
 }
