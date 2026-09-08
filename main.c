36e9c88e976731210a4512cfa44ab1b32dfd70f6
lichiahaomc <lichiahao_mc@163.com>
2026-09-08 16:03:43 +0800

    学习了 \t 的用法

--- main.c
+++ main.c
@@ -1,17 +1,9 @@
 #include <stdio.h>
 
-int add(int a, int b);
-
 int main(void)
 {
-    int a, b;
-    scanf("%d %d", &a, &b);
-    printf("%d + %d = %d\n", a, b, add(a, b));
+    printf("Name\t\tID\n");
+    printf("LI Jiahao\t2026150083\n");
 
     return 0;
-}
-
-int add(int a, int b)
-{
-    return a + b;
 }
